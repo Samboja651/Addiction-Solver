d757b4796811e1eca5581e30b7e96ea03eba5d07
Samboja651 <waribekihiko651@gmail.com>
2024-01-26 15:53:03 +0300

    added layout for user story

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def Test():
-    return 'Set up environment'
+def test_stories():
+    return render_template('f_stories.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
