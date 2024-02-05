98ab58b35efa2ace70ba17d4e8b42d36a04a9e9a
Samboja651 <waribekihiko651@gmail.com>
2024-02-05 21:32:31 +0300

    added seed data for stories

--- seed.py
+++ seed.py
@@ -9,7 +9,13 @@ def seed_db():
     success_stories = [
         ("1", "Jane Wawuda", "I can now live a more productive life because with the help of this system I broke the chains of Alcoholism. I guarantee and recommend that you will have a progressive journey.", "http://localhost:5000/1"),
         ("2", "Abdallah Masoud", "After struggling with addiction for years, I decided to seek help. Through counseling and support from this addiction solver app, I successfully overcame my addiction. Today, I am living a healthier and happier life.", "http://localhost:5000/2"),
-        ("3", "John Majiba", "My journey with this application has helped me to practice self control against browsing unproductively on social media.", "https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute")
+        ("3", "John Majiba", "My journey with this application has helped me to practice self control against browsing unproductively on social media.", "http://localhost:5000/3"),
+        ("4", "Isaack Kulot", "I've tried so many times, but The Addiction Solver helped me make it work. After 10 years in addiction, my kids finally call me 'Daddy' again.", "http://localhost:5000/4"),
+        ("5", "Joanna Macabus", "Through the trusting guidance of the programs of Addiction Solver, I finally had the courage to go back and make peace with the demons inside me.", "http://localhost:5000/5"),
+        ("6", "Mirriam Nimo", "It felt like I search a million treatment centers, The Addiction Solver was the only one that helped me about what was happening to our daughter…and our family.", "http://localhost:5000/6"),
+        ("7", "William Namanu", "The Addiction Solver was the complete opposite of what I expected or had gone through in other programs. I found myself looking forward to sessions and feeling the weight lifting off of my shoulders with every new day. This is where I learned to deal with my problems properly instead of running away. I learned that I mattered.", "http://localhost:5000/7"),
+        ("8", "Kelvin Ogembo", "I'm 9 years sober thanks to Addiction Solver helping me reconnect to myself & God.", "http://localhost:5000/8"),
+        ("9", "Christine Ngandi", "In just 30 days, my doctor at Addiction Solver helped me find a special person I didn't realize lived inside of me.", "http://localhost:5000/9")
     ]
 
     # form_data = [
