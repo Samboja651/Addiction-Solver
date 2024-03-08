c8e6d1f8c3087eb5ca91575ca68cc8d9f31e132f
Samboja651 <waribekihiko651@gmail.com>
2024-03-08 13:50:51 +0300

    educational resources from database, please see the readme file

--- app/routes/eresource.py
+++ app/routes/eresource.py
@@ -17,10 +17,10 @@ def resource():
     label = request.form['label']
     if label == 'All':
         eresources = db.execute("SELECT title, body, article_link, video_name, video_link FROM educational_resources").fetchall()
-        return render_template('eresource/content.html', eresources = eresources, labels = addictions)
+        return render_template('eresource/content.html', eresources = eresources, labels = addictions, label = label)
     
     eresources = db.execute("SELECT title, body, article_link, video_name, video_link FROM educational_resources WHERE type_id IN (SELECT type_id FROM addiction WHERE addiction_type = ?)", (label,)).fetchall()
 
-    return render_template('eresource/content.html', eresources = eresources, labels = addictions)
+    return render_template('eresource/content.html', eresources = eresources, labels = addictions, label = label)
 
 
